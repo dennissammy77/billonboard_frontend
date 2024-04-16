81dfd8220ca260139bd833c4be238a44c0206bde
dennissammy77 <dennissammy77@gmail.com>
2024-04-16 23:19:02 +0300

    Made changes to the password reset api endpoints

--- src/api/auth/password/route.js
+++ src/api/auth/password/route.js
@@ -1,14 +1,57 @@
 import Handler from "@/api/routehandler";
 import axios from "axios";
+import Cookies from 'universal-cookie';
 
-let base_url = await Handler();
 
-export async function Password_Reset(payload) {
-    const result = await axios.put(`${base_url}/api/password_reset`,payload);
-    return result;
-}
-
-export async function EmailOtp(payload) {
-    const result = axios.post(`${base_url}/api/otp/send/verify_email`,payload);
-    return result;
-}
+export async function SEND_OTP_CODE_TO_USER (EMAIL){
+    let BASE_URL = await Handler();
+    const cookies = new Cookies();
+    let CONFIG = {
+      method: 'get',
+      maxBodyLength: Infinity,
+      url: `${BASE_URL}/api/auth/password/code/${EMAIL}`,
+      headers: { }
+    };
+  
+    const result = await axios.request(CONFIG).then((response) => {
+        return response;
+      }).catch((error) => {
+        return(error)
+      });
+    if (result?.data?.error){
+        return result;
+    }else{
+        cookies.set('reset_code_token', result?.data.token, {path: '/'});
+        return result;
+    }
+  }
+  export async function USER_PASSWORD_RESET_TO_NEW (data,EMAIL){
+    let BASE_URL = await Handler();
+    const cookies = new Cookies();
+  
+    const CODE_TOKEN = cookies.get('reset_code_token');
+  
+  
+    let CONFIG = {
+      method: 'put',
+      maxBodyLength: Infinity,
+      url: `${BASE_URL}/api/auth/password/new/${EMAIL}`,
+      headers: { 
+        'Authorization': `Bearer ${CODE_TOKEN}`
+      },
+      data: data
+    };
+  
+    const result = await axios.request(CONFIG).then((response) => {
+        return response;
+      }).catch((error) => {
+        return(error)
+      });
+      if (result?.data?.error){
+          return result;
+      }else{
+          cookies.remove('user_token1', { path: '/' });
+          cookies.remove('reset_code_token', { path: '/' });
+          return result;
+      }
+  }
